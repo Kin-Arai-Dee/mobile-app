2912a62c34ebd04065ca39df74c6935a001cc9d9
xeakkyx <soravit34642@hotmail.com>
2022-03-13 21:30:17 +0700

    user James

---
+++ src/dto/user.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  username: string
+  mail: string
+  profilePic: string
+  age: number
+  weight: number
+  height: number
+  favoriteFood: string[]
+  freqFood: string
+  banFood: string[]
+}
---
+++ src/mocks/user.ts
@@ -0,0 +1,14 @@
+import { IUser } from './../dto/user';
+
+export const mockFood: IUser = {
+  username: 'James',
+  mail: 'yugijamedi@outlook.com',
+  profilePic:
+    'https://cdn.discordapp.com/attachments/906634604860039168/906634663815184394/IMG_4991.JPG',
+  age: 21,
+  weight: 53,
+  height: 176,
+  favoriteFood: ['ไก่ทอด', 'พิซซ่า', 'ซูชิ'],
+  freqFood: 'ซูชิ',
+  banFood: ['กุ้ง'],
+}
